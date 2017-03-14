fb9a5ef1f65678b5d5da0108a05d4ca72ac6f4f2
Eric Martindale <eric@ericmartindale.com>
2017-03-13 20:09:35 -0700

    Add basic schema design

--- etymog.js
+++ etymog.js
@@ -3,4 +3,48 @@ var config = require('./config');
 var Maki = require('maki');
 var etymog = new Maki(config);
 
+etymog.define('Entry', {
+  attributes: [
+    text: { type: String , required: true },
+    language: { type: String , enum: ['dari', 'english'] },
+    translations: [ { type: String , ref: 'Entry' } ],
+    cognates: [ { type: String , ref: 'Entry' } ],
+  ]
+});
+
+etymog.define('Language', {
+  attributes: {
+    name: { type: String , required: true , slug: true , id: true },
+    family: { type: String , ref: 'Family' }
+  }
+});
+
+etymog.define('Family', {
+  attributes: {
+    name: { type: String , required: true , slug: true , id: true }
+  }
+});
+
+etymog.define('User', {
+  attributes: [
+    username: { type: String , required: true }
+  ]
+});
+
+etymog.define('Index', {
+  public: false,
+  name: 'Index',
+  templates: {
+    query: 'splash'
+  },
+  components: {
+    masthead: 'maki-pitch',
+    query: 'etymog-splash',
+    get: 'maki-splash'
+  },
+  routes: {
+    query: '/'
+  }
+});
+
 etymog.start();
