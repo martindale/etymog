c4d74be5a0326ea0c352703569558b6c59e74411
Janet Ghazizadeh <janet.ghazizadeh@nccgroup.trust>
2017-03-24 20:03:10 -0700

    Adding headline

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
     pitch: 'the world\'s dictionary',
     mission: 'Maki makes building beautiful apps a breeze.',
     description: 'Maki allows you to focus on your project, rather than laboring over architecture decisions.  With true isomorphism, you control how your application behaves, and can then customize how your features are exposed on specific platforms.',
-    about: 'Maki is a labor of love, a result of several years of iterations and re-architectures in pursuit of a clean, hand-rolled framework for building beautiful applications.  We hope you like it.',
+    about: 'Qatra, qatra darya mesha. Drop by drop, the rivers of knowledge are formed. Etymog is a slow and steady distributed platform for people around the world to contribute their knowledge. Those who are thirsty are welcome to take a drink.',
     copyright: 'Maki is copy<em>left</em>, and encourages you to copy, clone, and <em>create</em>.  After all, without a rich public domain, how else can we innovate?  Much to our chagrin, the software itself is more formally <a href="https://github.com/martindale/maki/blob/master/LICENSE">MIT licensed</a>, while our content is licensed under <a href="http://creativecommons.org/licenses/by/4.0/">CC-BY</a>.',
     //masthead: '/img/breeze.gif',
     cta: {
